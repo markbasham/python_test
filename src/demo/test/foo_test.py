73f74f69e4bbec94fecf72d9f570b17c812c81cf
Mark Basham <mark.basham@diamond.ac.uk>
2015-03-25 22:24:52 +0000

    Added a test for foo

---
+++ src/demo/test/foo_test.py
@@ -0,0 +1,29 @@
+'''
+Created on 25 Mar 2015
+
+@author: ssg37927
+'''
+import unittest
+
+
+class Test(unittest.TestCase):
+
+    def testadd(self):
+        from demo import foo
+        self.assertEqual(foo.add(1, 2), 3)
+
+    def testsub(self):
+        from demo import foo
+        self.assertEqual(foo.sub(5, 2), 3)
+
+    def testmul(self):
+        from demo import foo
+        self.assertEqual(foo.mul(1, 2), 2)
+
+    def testdiv(self):
+        from demo import foo
+        self.assertEqual(foo.div(10, 2), 5)
+
+
+if __name__ == "__main__":
+    unittest.main()
