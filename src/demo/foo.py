fd5b4fbea896532a244fe225dcd6e5a98732ffb9
Mark Basham <mark.basham@diamond.ac.uk>
2015-03-25 21:59:49 +0000

    Added some simple code

---
+++ src/demo/foo.py
@@ -0,0 +1,18 @@
+'''
+Created on 25 Mar 2015
+
+@author: ssg37927
+'''
+
+
+def add(a, b):
+    return a+b
+
+def sub(a, b):
+    return a-b
+
+def mul(a, b):
+    return a*b
+
+def div(a, b):
+    return a/b
